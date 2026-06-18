abb06408fa9ffe5c844a1b85a12612fcff8ccc33
Debabrata24 <mr.debabrtapc2006@gmail.com>
2026-06-18 20:03:11 +0530

    versel_tag_added

--- font/src/App.jsx
+++ font/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { Analytics } from "@vercel/analytics/react"
 import Header from './component/Header'
 import Footer from './component/Footer'
 import Main from './component/Main'
@@ -6,6 +7,7 @@ import Main from './component/Main'
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
+      <Analytics/>
       <Header />
 
       <main className="flex-1">
